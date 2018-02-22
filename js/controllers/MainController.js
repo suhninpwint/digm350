49153492d6eb9fd4c0d878d625e2a7ba18fbca54
Kaity <kaitlynn281@gmail.com>
2018-02-22 17:33:53 -0500

    V1

---
+++ js/controllers/MainController.js
@@ -0,0 +1,38 @@
+app.controller('MainController', ['$scope', function($scope) {
+    $scope.mainplaces = [
+    {
+        title: 'Baby Blues BBQ',
+        img: 'img/#',
+        bio: "Baby Blue's BBQ offers an array of exceptional meats and sides. Only a quick walk away from Drexel's campus. Be sure to check out the adjacent restaraunts around this gem of a place."
+    },
+    {
+        title: 'Cucina Zapata',
+        img: 'img/#',
+        bio: 'words words words words words'
+    },
+    {
+        title: 'Happy Sunshine',
+        img: 'img/#',
+        bio: 'words words words words words'
+    },
+    {
+        title: 'Wahoos',
+        img: 'img/#',
+        bio: 'words words words words words'
+    },
+    {
+        title: "Trader Joe's",
+        img: 'img/#',
+        bio: 'words words words words words'
+    },
+    {
+        title: 'Fresh Grocer',
+        img: 'img/#',
+        bio: 'words words words words words'
+    }
+    ]
+    
+    
+    
+    
+}]);
