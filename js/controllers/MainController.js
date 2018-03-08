04dcb6f6ab8a6138d9c7f1bd8902c55bfd369d5e
Kaity <kaitlynn281@gmail.com>
2018-03-08 13:11:32 -0500

    adding sequence.js

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -24,38 +24,48 @@ app.controller('MainController', ['$scope', function($scope) {
             title: 'Convenience'
     }
         
-    ]
+    ],
     $scope.mainplaces = [
     {
         title: 'Baby Blues BBQ',
         img: 'img/#',
-        bio: "Baby Blue's BBQ offers an array of exceptional meats and sides. Only a quick walk away from Drexel's campus. Be sure to check out the adjacent restaraunts around this gem of a place."
+        bio: "Baby Blue's BBQ offers an array of exceptional meats and sides. Only a quick walk away from Drexel's campus. Be sure to check out the adjacent restaraunts around this gem of a place.",
+        num: 1
     },
     {
         title: 'Cucina Zapata',
         img: 'img/#',
-        bio: 'words words words words words'
+        bio: 'words words words words words',
+        num: 2
     },
     {
         title: 'Happy Sunshine',
         img: 'img/#',
-        bio: 'words words words words words'
+        bio: 'words words words words words',
+        num: 3
     },
     {
         title: 'Wahoos',
         img: 'img/#',
-        bio: 'words words words words words'
+        bio: 'words words words words words',
+        num: 4
     },
     {
         title: "Trader Joe's",
         img: 'img/#',
-        bio: 'words words words words words'
+        bio: 'words words words words words',
+        num: 5
     },
     {
         title: 'Fresh Grocer',
         img: 'img/#',
-        bio: 'words words words words words'
+        bio: 'words words words words words',
+        num: 6
     }
-    ]
+    ],
+    //function that returns a string which will be the class name 
+    //increments the number depending on the length of the loop
+  
+    
      
 }]);
