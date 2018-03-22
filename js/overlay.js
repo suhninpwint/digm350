37a04b9de5fe02379564da7c73c14dc05b571d5f
Kaity <kaitlynn281@gmail.com>
2018-03-22 10:48:31 -0400

    added the if statement

--- js/overlay.js
+++ js/overlay.js
@@ -3,6 +3,14 @@ var bio = document.getElementById('bio-container');
 var exit = document.getElementById('close-container');
 var overlay = document.getElementById('overlay-container');
 
+//Info buttons
+var b1 = document.getElementById('button1');
+var b2 = document.getElementById('button2');
+var b3 = document.getElementById('button3');
+var b4 = document.getElementById('button4');
+var b5 = document.getElementById('button5');
+var b6 = document.getElementById('button6');
+
 function showOverlay(){
     overlay.classList.remove('hidden');
     overlay.classList.add('active');
@@ -11,4 +19,22 @@ function showOverlay(){
 function hideOverlay(){
     overlay.classList.remove('active');
     overlay.classList.add('hidden');
+}
+
+function writeInfo(button){
+    var id = button.id;
+    
+    if(id == 'button1'){
+        console.log('1');
+    }  else if(id == 'button2'){
+        console.log('2');
+    }  else if(id == 'button3'){
+        console.log('3');
+    }  else if(id == 'button4'){
+        console.log('4');
+    }  else if(id == 'button5'){
+        console.log('5');
+    }  else if(id == 'button6'){
+        console.log('6');
+    }
 }
